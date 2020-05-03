e53ac11f71ebbfc42aefde7951f51a7f97e671c3
Preksha Pansheriya <56806702+ppansheriya@users.noreply.github.com>
2020-05-03 13:00:19 -0700

    Add files via upload

---
+++ BraceExpansion.java
@@ -0,0 +1,58 @@
+Time Complexity = O(k^n/k)
+Space Complexity = O(k)
+
+
+class Solution{
+    List<String> result;
+
+     public String[] expand(String S){
+         int cursor = 0;
+         result = new ArrayList<>();
+         List<<List<Character>>> blocks = new ArrayList<>();
+         
+         while(cursor < S.length()){
+             List<Character> currentBlock = new ArrayList<>();
+             
+             if(S.charAt(cursor) == '{'){
+                 cursor++;
+                  while(S.charAt(cursor) != '}'){
+                        if(S.charAt(cursor) != ','){
+                          currentBlock.add(S.charAt(cursor);
+                        cursor++;
+                      }
+                  }
+             }else{
+                 currentBlock.add(S.charAt(cursor);
+             }
+             blocks.add(currentBlock);
+             cursor++;
+             
+         }
+         
+         backtrack(blocks,0,new StringBuilder());
+         
+         Collections.sort(result);
+         
+         String[] arr = new String(result);
+         
+         for(int i = 0 ; i < result.size();i++){
+             arr[i] = result.get(i);
+         }
+         return arr;
+         
+     }
+     
+     private void backtrack(List<List<Character>>> blocks, int index, StringBuilder sb){
+         if(index == blocks.){
+             result.add(sb.toString());
+             return;
+         }
+         
+         
+         for(char c: blocks.get(index)){
+             sb.append(c);
+             backtrack(blocks,index+1,sb);
+             sb.deleteCharAt(sb.length() - 1);
+         }
+     }
+}
