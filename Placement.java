1f83f4ebabcda498e3e2ef10580a2517d0777b26
Yasunori Iwayanagi <yiwayanagi@upward.net>
2021-04-02 09:34:25 -0700

    Submission backtracking 4

---
+++ Placement.java
@@ -0,0 +1,78 @@
+//Time Complexity:  n^(H*W)
+//Space Complexity: O(HW) 
+
+import java.util.*;
+
+class OptimalPlacement{
+    static int min = Integer.MAX_VALUE;
+    public static void main(String[] args){
+        System.out.println(findMinDist(4,4,3));
+    }
+
+    public static int findMinDist(int H,int W,int n){
+        int[][] grid = new  int[H][W];
+
+        for(int i=0;i<H;i++){
+            for(int j=0;j<W;j++){
+                grid[i][j] = -1;
+            }
+        }
+        backTrack(grid,0,0,n,H,W);
+        return min;
+    }
+    private static void bfs(int[][] grid,int H,int W){
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] visited =  new boolean[H][W];
+        for(int i=0;i<H;i++){
+            for(int j=0;j<W;j++){
+                if(grid[i][j] == 0){
+                    q.add(new int[]{i,j});
+                    visited[i][j] = true;
+                }
+                    
+            }
+        }
+        int dirs[][] = {{0,1},{0,-1},{1,0},{-1,0}};
+        int dist =0;
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                int[] curr = q.poll();
+                for(int[] dir : dirs){
+                    int r = dir[0] + curr[0];
+                    int c = dir[1] + curr[1];
+                    if(r>=0 && r<H && c>=0 && c<W && !visited[r][c]){
+                        
+                        visited[r][c] = true;
+                        q.add(new int[]{r,c});
+                    }
+                }
+            }
+            dist++;
+        }
+
+        min = Math.min(dist-1,min);
+
+    }
+    private static void backTrack(int[][] grid,int r,int c,int n,int H,int W){
+        //Base case
+        if(n == 0){
+            bfs(grid,H,W);
+            return;
+        }
+        //Logic
+        for(int i=r;i<H;i++){
+            for(int j=c;j<W;j++){
+                grid[i][j] = 0; //action
+                if(j<W){
+                    backTrack(grid,i,j+1,n-1,H,W); 
+                }
+                else{
+                    backTrack(grid,i+1,0,n-1,H,W); 
+                }
+                grid[i][j] = -1; //backtrack
+            }
+        }
+        
+    }
+}
